9634eace2f44de801ade0e389da95a495afa0d47
IQbrod <iqbrod@outlook.fr>
2019-04-23 10:12:19 +0200

    Fix merge avec maven

--- src/com/excilys/cdb/exception/FailedSQLQueryException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.cdb.exception;
-
-public class FailedSQLQueryException extends Exception {
-	private static final long serialVersionUID = 19042019L;
-
-	public FailedSQLQueryException(String query) {
-		super("["+query+"] failed");
-	}
-}
--- src/com/excilys/cdb/exception/InvalidIdException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.cdb.exception;
-
-public class InvalidIdException extends Exception {
-	private static final long serialVersionUID = 17042019L;
-
-	public InvalidIdException(int id) {
-		super("Invalid id: "+id);
-	}
-}
