49e9d006c17e0735632ee1540aa8751fb3360cb3
IQbrod <iqbrod@outlook.fr>
2019-04-26 12:00:26 +0200

    Test Service

---
+++ src/test/java/com/excilys/cdb/service/CompanyServiceTest.java
@@ -0,0 +1,44 @@
+package com.excilys.cdb.service;
+
+import org.junit.*;
+
+import com.excilys.cdb.dto.CompanyDto;
+import com.excilys.cdb.exception.*;
+
+public class CompanyServiceTest {
+
+	@Test
+	public void InstanceTest() throws DatabaseProblemException {
+		CompanyService.getInstance();
+	}
+	
+	@Test
+	public void CreateTest() throws Exception {
+		CompanyService.getInstance().create(new CompanyDto("80","Entreprise"));
+	}
+	
+	@Test
+	public void ReadTest() throws Exception {
+		CompanyService.getInstance().read("80");
+	}
+	
+	@Test
+	public void UpdateTest() throws Exception {
+		CompanyService.getInstance().update(new CompanyDto("5","Updated"));
+	}
+	
+	@Test
+	public void DeleteTest() throws Exception {
+		CompanyService.getInstance().delete(new CompanyDto("80","DeleteMe"));
+	}
+	
+	@Test
+	public void listAllTest() throws Exception {
+		CompanyService.getInstance().listAllElements();
+	}
+	
+	@Test
+	public void listTest() throws Exception {
+		CompanyService.getInstance().list("1", "20");
+	}
+}
---
+++ src/test/java/com/excilys/cdb/service/ComputerServiceTest.java
@@ -0,0 +1,44 @@
+package com.excilys.cdb.service;
+
+import org.junit.*;
+
+import com.excilys.cdb.dto.ComputerDto;
+import com.excilys.cdb.exception.*;
+
+public class ComputerServiceTest {
+
+	@Test
+	public void instanceTest() throws DatabaseProblemException {
+		ComputerService.getInstance();
+	}
+	
+	@Test
+	public void createTest() throws Exception {
+		ComputerService.getInstance().create(new ComputerDto("800","Ordinateur",null,null,"0"));
+	}
+	
+	@Test
+	public void readTest() throws Exception {
+		ComputerService.getInstance().read("800");
+	}
+	
+	@Test
+	public void updateTest() throws Exception {
+		ComputerService.getInstance().update(new ComputerDto("5","Updated",null,null,"0"));
+	}
+	
+	@Test
+	public void deleteTest() throws Exception {
+		ComputerService.getInstance().delete(new ComputerDto("800","DeleteMe",null,null,"0"));
+	}
+	
+	@Test
+	public void listAllTest() throws Exception {
+		ComputerService.getInstance().listAllElements();
+	}
+	
+	@Test
+	public void listTest() throws Exception {
+		ComputerService.getInstance().list("1", "20");
+	}
+}
