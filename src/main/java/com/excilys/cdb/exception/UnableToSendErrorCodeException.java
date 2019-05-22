2c69fc1d3b2a368242f013aa7948cb7834cba7d8
IQbrod <iqbrod@outlook.fr>
2019-05-22 11:14:18 +0200

    Remove useless exceptions & does not test spring config

--- src/main/java/com/excilys/cdb/exception/UnableToSendErrorCodeException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.cdb.exception;
-
-public class UnableToSendErrorCodeException extends ShouldOnlyBeLoggedException {
-	private static final long serialVersionUID = 9052019L;
-	
-	public UnableToSendErrorCodeException(int code) {
-		super("Could not send http status: "+ code);
-	}
-}
--- src/main/java/com/excilys/cdb/exception/UnexpectedServletException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.cdb.exception;
-
-public class UnexpectedServletException extends ShouldOnlyBeLoggedException {
-	private static final long serialVersionUID = 9052019L;
-	
-	public UnexpectedServletException(String servletName, String method) {
-		super("Something went wrong with the servlet: "+ servletName +" on method "+ method);
-	}
-}
